5f884514617ff06b73900567f9c470c4320c9ddb
sohan <sohanprasad2003@gmail.com>
2025-01-31 09:29:53 +0530

    remove two class files

---
+++ Arrays.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a size of array : ");
+        int s = sc.nextInt();
+        int[] arr = new int[s];
+
+        int i;
+        for(i = 0; i < arr.length; ++i) {
+            System.out.print("enter a " + i + " index value : ");
+            int num = sc.nextInt();
+            sc.nextLine();
+            arr[i] = num;
+        }
+
+        for(i = 0; i < arr.length; ++i) {
+            System.out.print("" + i + " ");
+        }
+    }
+}
